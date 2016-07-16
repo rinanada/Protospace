f8905bdd06a746020ba74c9d4ece70212c305aeb
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-07-16 13:57:50 +0900

    %a →　=link_to, nickname/password/pro_imgカラムの追加

--- db/migrate/20160716034123_devise_create_users.rb
+++ db/migrate/20160716034123_devise_create_users.rb
@@ -18,7 +18,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
+     add_column :users, :password, :string
 
+     add_column :users, :nickname, :string
+
+     add_column :users, :pro_img, :string
       ## Confirmable
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
