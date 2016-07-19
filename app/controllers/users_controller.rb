7d0f771b66bc72b6db3ea27bfc61bd100d554fc6
daijiro <futbolvalent15@gmail.com>
2016-07-19 15:54:20 +0900

    users_controller.rbのuser_paramsメソッドで定義したストロングパラメーターからpassword confirmationを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
 
     private
     def user_params
-      params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
+      params.require(:user).permit(:nickname, :email, :password)
     end
 end
