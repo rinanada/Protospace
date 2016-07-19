af4e060ae28e211e0a2742212d10950dce0b7714
daijiro <futbolvalent15@gmail.com>
2016-07-19 16:51:46 +0900

    users_controllersの空行インデントを調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def update
     current_user.update(user_params)
     sign_in(current_user, bypass: true)
-    redirect_to  root_path
+    redirect_to root_path
   end
 
   private
