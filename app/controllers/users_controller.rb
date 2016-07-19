4d12d5819e5452a419d55ad9fd534d3ec788c41d
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-07-19 14:39:12 +0900

    editからindexへの処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-      params.require(:user).permit(:nickname, :email, :password)
+      params.require(:user).permit(:nickname, :email, :password).merge(id: params[:id])
     end
 
 
