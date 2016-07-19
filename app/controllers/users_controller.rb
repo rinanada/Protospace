ecbd032929b8f9fb48441956144714b6c72a9c09
daijiro <futbolvalent15@gmail.com>
2016-07-19 16:47:47 +0900

    users_controller.rbの空行、インデントを調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
 class UsersController < ApplicationController
-   before_action :authenticate_user!
+  before_action :authenticate_user!
 
+  def show
+  end
 
-    def edit
+  def edit
+  end
 
-    end
+  def update
+    current_user.update(user_params)
+    sign_in(current_user, bypass: true)
+    redirect_to  root_path
+  end
 
+  private
 
-    def update
-      current_user.update(user_params)
-      sign_in(current_user, bypass: true)
-      redirect_to  root_path
-    end
-
-    private
-    def user_params
-      params.require(:user).permit(:nickname, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:nickname, :email, :passwor)
+  end
 end
