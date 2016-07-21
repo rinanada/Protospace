f7522ee2ac9beacd3df51f42d27a7464b35cc753
daijiro <futbolvalent15@gmail.com>
2016-07-21 15:03:25 +0900

    carrierwaveを使ってcaptureimagesテーブルのcontentカラムにファイルをアップロードした際にファイル名を表示

--- app/models/capture_image.rb
+++ app/models/capture_image.rb
@@ -1,7 +1,9 @@
 class CaptureImage < ActiveRecord::Base
 
   enum type: { main: 0, sub: 1 }
-
   self.inheritance_column = :_type_disabled
   belongs_to :prototype
+  mount_uploader :content, ImageUploader
+
+
 end
