4480c02bb57b3bdc217820e91d4882acb7f4ec17
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-07-22 20:11:43 +0900

    部分テンプレートのcollection、localsの使用

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,6 +3,8 @@ class Prototype < ActiveRecord::Base
   has_many :capture_images, dependent: :destroy
   accepts_nested_attributes_for :capture_images, reject_if: :subimage
   validates :title, :concept, :catch_copy, presence: true
+  has_one :main_image, 
+    class_name: "capture_images"
 
   def subimage(attributed)
     attributed['content'].blank? if attributed.has_value?('sub')
