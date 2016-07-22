67237f2a891ab7c3ac3eb18d52121a863b66c460
daijiro <futbolvalent15@gmail.com>
2016-07-22 15:50:27 +0900

    validationの記述をまとめました

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,10 +2,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :capture_images, dependent: :destroy
   accepts_nested_attributes_for :capture_images, reject_if: :subimage
-
-  validates :title, presence: true
-  validates :concept, presence: true
-  validates :catch_copy, presence: true
+  validates :title, :concept, :catch_copy, presence: true
 
   def subimage(attributed)
     attributed['content'].blank? if attributed.has_value?('sub')
