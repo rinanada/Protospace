b2f5de6ef2f1ae615a955719aa43dd217f03c274
daijiro <futbolvalent15@gmail.com>
2016-07-25 17:39:18 +0900

    comment_paramsを定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,11 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment_text).merge(user_id: current_user.id)
   end
 end
