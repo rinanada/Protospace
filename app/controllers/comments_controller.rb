56a5c0ddc11ca7e12b9dbbfef62aff48c36a8fda
daijiro <futbolvalent15@gmail.com>
2016-07-25 17:47:40 +0900

    createアクションにredirectをセット

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,11 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.save
+    redirect_to prototype_path(@comment.prototype)
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:comment_text).merge(user_id: current_user.id)
+    params.require(:comment).permit(:comment_text).merge(prototype_id: params[:prototype_id], user_id: current_user.id)
   end
 end
