395f111ff2bd2377099779cdf3a01fc33e18e616
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-07-26 17:47:21 +0900

    ルーティングにdestroyメソッドの追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :prototypes do
     resources :comments, only: [:create]
-    resources :likes, only: [:create]
+    resources :likes, only: [:create, :destroy]
   end
 end
