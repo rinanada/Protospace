f05f0034aee64ed95445e305cec358d5bb389287
daijiro <futbolvalent15@gmail.com>
2016-07-26 22:04:14 +0900

    コントローラー周り、_likeテンプレート、js周りを調整

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,15 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @like =Like.create(user_id: current_user.id, prototype_id: params[:id])
-    @likes = Like.where(prototype_id: params[:id])
+    binding.pry
+    @like =Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @likes = Like.where(prototype_id: params[:prototype_id])
     @prototype = Prototype.find(params[:id])
   end
 
   def destroy
     Like.find_by(user_id: current_user.id).destroy
-    @likes = Like.where(prototype_id: params[:id])
-    @prototype = Prototype.find(params[:id])
+    @likes = Like.where(prototype_id: params[:prototype_id])
+    @prototype = Prototype.find(params[:prototype_id])
   end
 end
