e0909cc487d7d617be5d53a60f348dff0df7eb1f
daijiro <futbolvalent15@gmail.com>
2016-07-26 20:07:32 +0900

    likeコントローラーを編集

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,15 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    @likes = Like.new
+    @like =Like.create(user_id: current_user.id, prototype_id: params[:id])
+    @likes = Like.where(prototype_id: params[:id])
+    @prototype = Prototype.find(params[:id])
   end
 
   def destroy
-    @likes
+    Like.find_by(user_id: current_user.id).destroy
+    @likes = Like.where(prototype_id: params[:id])
+    @prototype = Prototype.find(params[:id])
   end
 end
