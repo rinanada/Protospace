eef6423514ea4a8e26805afdd958c2e1cadf5ebe
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-07-26 14:16:43 +0900

    testを作らないための記述

--- config/application.rb
+++ config/application.rb
@@ -22,11 +22,11 @@ module Protospace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework    nil
+    end
   end
-
-    #   config.generators do |g|
-    #   g.stylesheets false
-    #   g.javascripts false
-    #   g.helper false
-    # end
 end
