1272f32f565c84c9348fc9ed45b606999f27de70
daijiro <futbolvalent15@gmail.com>
2016-07-28 13:07:38 +0900

    likesコントローラーのアクション編集

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,16 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    binding.pry
     @like =Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
     @likes = Like.where(prototype_id: params[:prototype_id])
-    @prototype = Prototype.find(params[:id])
+    @prototype = Prototype.find(params[:prototype_id])
+    # redirect_to prototype_path(@prototype)
   end
 
   def destroy
-    Like.find_by(user_id: current_user.id).destroy
+    Like.find_by(prototype_id: params[:prototype_id], user_id: current_user.id).destroy
     @likes = Like.where(prototype_id: params[:prototype_id])
     @prototype = Prototype.find(params[:prototype_id])
+    # redirect_to prototype_path(@prototype)
   end
 end
