f27fef950c1610eb3363519c2566fce97949ec01
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-07-29 15:27:14 +0900

    手直し

--- app/controllers/prototypes/newest_controller.rb
+++ app/controllers/prototypes/newest_controller.rb
@@ -1,5 +1,4 @@
 class Prototypes::NewestController < ApplicationController
-before_action :authenticate_user!
 
   def index
     @prototypes = Prototype.includes(:user, :capture_images).order("created_at DESC").page(params[:page]).per(8)
