26addf209f01df7f1b35bbac83e4472f93a2fc25
daijiro <futbolvalent15@gmail.com>
2016-07-29 18:35:00 +0900

    js実装の試行

--- app/assets/javascripts/main_preview.js
+++ app/assets/javascripts/main_preview.js
@@ -1,54 +1,22 @@
+$(document).on("change", "#prototype_capture_images_attributes_0_content", function() {
 
-// Fileオブジェクトを内包するモデル
-var FileModel = Backbone.Model.extend({
- 
-  defaults: {
-    file: '',
-    dataURL: ''
-  },
- 
-  initialize: function(attr) {
-    var self = this;
- 
-    // FileReaderを準備しておく
-    self.reader = new FileReader();
-    self._eventify();
-  },
- 
-  _eventify: function() {
-    var self = this;
- 
-    // ファイルの読み込みが完了したらdataURLに値をセット
-    // その後、readerLoadイベントを発行する
-    self.reader.addEventListener('load', function() {
-      self.set('dataURL', self.reader.result);
-      self.trigger('readerLoad', self);
-    }, false);
-  },
- 
-  // ファイルを読み込むメソッド
-  readFile: function() {
-    var self = this;
-    var reader = self.reader;
- 
-    reader.readAsDataURL(this.get('file'));
-  }
- 
-});
+    var file = this.files;
 
-// -----------------------------------------------------------
+    // 画像ファイル以外は処理を中止
+    if (!file[0].type.match(/^image\/(png|jpeg|gif)$/)) return;
 
-var FileCollection = Backbone.Collection.extend({
- 
-  model: FileModel,
- 
-  // それぞれのModelのreadFileコールする
-  readFiles: function() {
-    var self = this;
- 
-    this.each(function(file) {
-      file.readFile();
-    });
-  }
- 
+    var img    = new Image();          // 表示用のイメージオブジェクトを取得
+    var reader = new FileReader();  // ファイルリーダー作成
+
+    // 画像がロードされてから処理を行う
+    reader.onload = function (e) {
+
+        // readAsDataURLをsrcに指定してイメージを作成
+        $("#prototype_capture_images_attributes_0_content").append("<img src='"+e.target.result+"' alt='canvas' />");
+
+    }
+
+    // readAsDataURLメソッドでファイルの内容を取得
+    reader.readAsDataURL(file[0]);   
+
 });
