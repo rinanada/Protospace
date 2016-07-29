bb93fd53143f6fdfef269940268c20f2352ca709
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-07-29 16:32:04 +0900

    スコープのメソッド名変更

--- app/controllers/prototypes/newest_controller.rb
+++ app/controllers/prototypes/newest_controller.rb
@@ -1,7 +1,7 @@
 class Prototypes::NewestController < ApplicationController
 
   def index
-    @prototypes = Prototype.with_proto.order("created_at DESC").page(params[:page]).per(8)
+    @prototypes = Prototype.including.order("created_at DESC").page(params[:page]).per(8)
   end
 
 end
