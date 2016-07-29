f0a37b731046a4e4bd0788d97cb372dbaa7fa722
daijiro <futbolvalent15@gmail.com>
2016-07-29 23:16:47 +0900

    プレビュー画像表示のためのjs記述

--- app/assets/javascripts/main_preview.js
+++ app/assets/javascripts/main_preview.js
@@ -1,21 +1,60 @@
-$(document).on("change", "#main_image", function() {
-  var inputFile = document.getElementById('main_image');
-  var reader = new FileReader();
-   
-  function fileChange(ev) {
-    var target = ev.target;
-    var file = target.files[0];
-    var type = file.type;
-    var size = file.size;
-   
-   
-    reader.readAsDataURL(file);
-  }
-   
-  function fileLoad() {
-    console.log(reader.result);
-  }
-   debugger
-  inputFile.addEventListener('change', fileChange, false);
-  reader.addEventListener('load', fileLoad, false);
+$(function() {
+  document.getElementById( "main_image" ).addEventListener( "change", function() {
+    // フォームで選択された全ファイルを取得
+    var fileList = this.files ;
+
+      // Blob URLの作成
+    var blobUrl = window.URL.createObjectURL( fileList[0] ) ;
+
+      // HTMLに書き出し (src属性にblob URLを指定)
+    document.getElementById("output_image").innerHTML += '<img src="' + blobUrl + '">' ;
+  } ) ;
+
+
+
+
+  document.getElementById( "sub_image1" ).addEventListener( "change", function() {
+    // フォームで選択された全ファイルを取得
+    var fileList = this.files ;
+
+      // Blob URLの作成
+    var blobUrl = window.URL.createObjectURL( fileList[0]);
+    debugger
+
+      // HTMLに書き出し (src属性にblob URLを指定)
+    document.getElementsByClassName("sub_output_image")[0].innerHTML += '<img src="' + blobUrl + '">' ;
+    debugger
+  });
+
+
+
+
+  document.getElementById( "sub_image2" ).addEventListener( "change", function() {
+    // フォームで選択された全ファイルを取得
+    var fileList = this.files ;
+
+      // Blob URLの作成
+    var blobUrl = window.URL.createObjectURL( fileList[0] ) ;
+    debugger
+
+      // HTMLに書き出し (src属性にblob URLを指定)
+    document.getElementsByClassName("sub_output_image")[1].innerHTML += '<img src="' + blobUrl + '">' ;
+  });
+
+
+
+
+
+  // document.getElementById( "form img" ).addEventListener( "change", function() {
+  //   // フォームで選択された全ファイルを取得
+  //   var fileList = this.files ;
+
+  //     // Blob URLの作成
+  //   var blobUrl = window.URL.createObjectURL( fileList[0] ) ;
+
+  //     // HTMLに書き出し (src属性にblob URLを指定)
+  //   document.getElementById("output_image").innerHTML += '<img src="' + blobUrl + '">' ;
+  // } ) ;
+
+
 });
