b3f033b0890c9f8ec5963616a262bb920c60ef3e
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-07-29 16:21:15 +0900

    コンロトーラーが複数作成されていたので消去

--- app/controllers/prototypes/newest_controller.rb
+++ app/controllers/prototypes/newest_controller.rb
@@ -1,7 +1,7 @@
 class Prototypes::NewestController < ApplicationController
 
   def index
-    @prototypes = Prototype.includes(:user, :capture_images).order("created_at DESC").page(params[:page]).per(8)
+    @prototypes = Prototype.with_proto.order("created_at DESC").page(params[:page]).per(8)
   end
 
 end
