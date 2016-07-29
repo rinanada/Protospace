d25d3f64fa86b6d9035de51fcb82b7c0e68fa280
灘　梨菜 <nada_rina@nadarina-no-MacBook-Air.local>
2016-07-29 18:59:43 +0900

    idをjsファイルにふってやり、consoleを確認するとjsを動かせていることは確認

--- app/assets/javascripts/main_preview.js
+++ app/assets/javascripts/main_preview.js
@@ -1,22 +1,10 @@
-$(document).on("change", "#prototype_capture_images_attributes_0_content", function() {
-
-    var file = this.files;
-
-    // 画像ファイル以外は処理を中止
-    if (!file[0].type.match(/^image\/(png|jpeg|gif)$/)) return;
-
-    var img    = new Image();          // 表示用のイメージオブジェクトを取得
-    var reader = new FileReader();  // ファイルリーダー作成
-
-    // 画像がロードされてから処理を行う
-    reader.onload = function (e) {
-
-        // readAsDataURLをsrcに指定してイメージを作成
-        $("#prototype_capture_images_attributes_0_content").append("<img src='"+e.target.result+"' alt='canvas' />");
-
-    }
-
-    // readAsDataURLメソッドでファイルの内容を取得
-    reader.readAsDataURL(file[0]);   
-
-});
+var inputFile = document.getElementById('prototype_capture_images_attributes_0_content');
+ 
+function fileChange(ev) {
+  var target = ev.target;
+  var files = target.files;
+ 
+  console.log(files);
+}
+ 
+inputFile.addEventListener('change', fileChange, false);
