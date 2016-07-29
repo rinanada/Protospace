f299ff5050cf37a16314cbc753f340f2c9310585
daijiro <futbolvalent15@gmail.com>
2016-07-29 19:10:22 +0900

    viewとjsの連携に成功

--- app/assets/javascripts/main_preview.js
+++ app/assets/javascripts/main_preview.js
@@ -1,10 +1,12 @@
-var inputFile = document.getElementById('prototype_capture_images_attributes_0_content');
- 
-function fileChange(ev) {
-  var target = ev.target;
-  var files = target.files;
- 
-  console.log(files);
-}
- 
-inputFile.addEventListener('change', fileChange, false);
+$(function() {
+  var inputFile = document.getElementById('prototype_capture_images_attributes_0_content');
+
+  function fileChange(ev) {
+    var target = ev.target;
+    var files = target.files;
+
+    console.log(files);
+  }
+
+  inputFile.addEventListener('change', fileChange, false);
+});
